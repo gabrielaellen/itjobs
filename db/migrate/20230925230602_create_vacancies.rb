6386270e3affbf9445d08949376215f2cb19a7b2
Gabriela <dev.gabrielaellen@gmail.com>
2023-09-25 20:43:57 -0300

    Create vacancy model

---
+++ db/migrate/20230925230602_create_vacancies.rb
@@ -0,0 +1,15 @@
+class CreateVacancies < ActiveRecord::Migration[7.0]
+  def change
+    create_table :vacancies do |t|
+      t.bigint :external_id, null: false
+      t.string :external_source, null: false
+      t.string :url, null: false
+      t.string :title, null: false
+      t.string :user_login, null: false
+      t.string :user_avatar_url, null: false
+      t.string :user_profile_url, null: false
+      t.jsonb :labels, null: false, default: '{}'
+      t.timestamps
+    end
+  end
+end
