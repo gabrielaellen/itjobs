96b82abeea2455f093c71f2a30d985e83c371eea
Gabriela <dev.gabrielaellen@gmail.com>
2023-10-04 13:42:21 -0300

    Add validations to the vacancy model

--- app/models/vacancy.rb
+++ app/models/vacancy.rb
@@ -1,2 +1,5 @@
 class Vacancy < ApplicationRecord
+    validates :external_id, :external_source, :url, :title, :user_login, :user_avatar_url, :user_profile_url, :labels, :body, presence: true
 end
+
+
