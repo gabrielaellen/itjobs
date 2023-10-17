56c3b7fa51a289165c41b11f6e6084fd8aa1c62d
Gabriela <dev.gabrielaellen@gmail.com>
2023-10-17 14:52:16 -0300

    Create populete vacancies job

---
+++ app/jobs/populate_vacancies_job.rb
@@ -0,0 +1,35 @@
+require "octokit"
+
+class PopulateVacanciesJob < ApplicationJob
+  queue_as :default
+
+  def perform(*args)
+    client = Octokit::Client.new
+
+    Vacancy::SOURCES.each do |key, value|
+
+      issues = client.issues(key, state: 'open')
+
+      issues.each do |issue|
+        vacancy = Vacancy.find_by(external_id: issue.id)
+
+        unless vacancy
+          vacancy = Vacancy.new
+          vacancy.external_id = issue.id
+          vacancy.external_source = key
+          vacancy.url = issue.url
+          vacancy.title = issue.title
+          vacancy.user_login = issue.user.login
+          vacancy.user_avatar_url = issue.user.avatar_url
+          vacancy.user_profile_url = issue.user.url 
+          vacancy.labels = issue.labels.to_json
+          vacancy.created_at = issue.created_at
+          vacancy.updated_at = issue.updated_at
+          vacancy.body = issue.body
+
+          vacancy.save!
+        end
+      end
+    end
+  end
+end
--- app/models/vacancy.rb
+++ app/models/vacancy.rb
@@ -1,4 +1,11 @@
 class Vacancy < ApplicationRecord
+  SOURCES = {
+    'frontendbr/vagas' => 'Front-End BR - Vagas',     
+    'backend-br/vagas' => 'Back-End BR - Vagas',
+    'react-brasil/vagas' => 'React BR - Vagas',
+    'qa-brasil/vagas' => 'QA BR - Vagas'
+  }.freeze
+
     validates :external_id, :external_source, :url, :title, :user_login, :user_avatar_url, :user_profile_url, :labels, :body, presence: true
 end
 
