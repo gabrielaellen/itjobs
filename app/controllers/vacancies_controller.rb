05c966b8c53afc6574392d2c9856476367c0200b
Gabriela <dev.gabrielaellen@gmail.com>
2023-10-19 14:34:39 -0300

    Create vacancy search presenter

--- app/controllers/vacancies_controller.rb
+++ app/controllers/vacancies_controller.rb
@@ -1,9 +1,15 @@
 class VacanciesController < ApplicationController
 
     def index
-        @vacancies = Vacancy.limit(6)
+        @vacancies = VacancySearchPresenter.new(query: params[:q], options: search_params).search
     end
 
     def show
+    end
+
+    private 
+    
+    def search_params
+        params.permit(:q, :page, :user, :label, :source)
     end
 end
---
+++ app/presenters/vacancy_search_presenter.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+class VacancySearchPresenter
+  attr_reader :options, :query
+
+  def initialize(query: nil, options: {})
+    @query    = query.presence || '*'
+    @options  = options
+  end
+
+  def search
+    constraints = {
+      page: options[:page],      
+      per_page: 20,            
+      fields: [:title]           
+    }
+
+    constraints[:where] = where
+    constraints[:order] = order
+    constraints[:aggs]  = aggs
+  
+    Vacancy.search(query, **constraints)
+  end
+
+  private
+
+  def where
+    where = {}
+
+    where[:user_login] = options[:user] if options[:user].present?
+    where[:labels] = options[:label] if options[:label].present?
+    where[:external_source] = options[:source] if options[:source].present?
+
+    where
+  end
+
+  def aggs
+    {
+      labels: { order: { _key: 'asc' } } 
+    }
+  end
+
+  def order
+    default = { created_at: :desc } 
+  end
+end
