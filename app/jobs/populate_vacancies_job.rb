d2911757f0400b3fb510d75da81744c37dac135b
Gabriela <dev.gabrielaellen@gmail.com>
2023-10-19 13:18:43 -0300

    adjust the vancancy job

--- app/jobs/populate_vacancies_job.rb
+++ app/jobs/populate_vacancies_job.rb
@@ -11,24 +11,22 @@ class PopulateVacanciesJob < ApplicationJob
       issues = client.issues(key, state: 'open')
 
       issues.each do |issue|
-        vacancy = Vacancy.find_by(external_id: issue.id)
+        vacancy = Vacancy.find_or_initialize_by(external_id: issue.id)
 
-        unless vacancy
-          vacancy = Vacancy.new
-          vacancy.external_id = issue.id
-          vacancy.external_source = key
-          vacancy.url = issue.html_url
-          vacancy.title = issue.title
-          vacancy.user_login = issue.user.login
-          vacancy.user_avatar_url = issue.user.avatar_url
-          vacancy.user_profile_url = issue.user.url 
-          vacancy.labels = issue.labels.to_json
-          vacancy.created_at = issue.created_at
-          vacancy.updated_at = issue.updated_at
-          vacancy.body = issue.body
+        vacancy.external_id = issue.id
+        vacancy.external_source = key
+        vacancy.url = issue.html_url
+        vacancy.title = issue.title
+        vacancy.user_login = issue.user.login
+        vacancy.user_avatar_url = issue.user.avatar_url
+        vacancy.user_profile_url = issue.user.url 
+        vacancy.labels = issue.labels.to_json
+        vacancy.created_at = issue.created_at
+        vacancy.updated_at = issue.updated_at
+        vacancy.body = issue.body
 
-          vacancy.save!
-        end
+        vacancy.save!
+      
       end
     end
   end
