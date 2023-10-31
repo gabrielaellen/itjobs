dc81680def47c6dafcf96d1fe19e2f05deaeaae8
Gabriela <dev.gabrielaellen@gmail.com>
2023-10-30 21:04:32 -0300

    Remove main.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,3 +4,6 @@ import "./scripts"
 import "./controllers"
 import * as bootstrap from "bootstrap"
 import '@fortawesome/fontawesome-free/js/all'
+import 'sneat-bootstrap-html-admin-template-free/assets/vendor/js/helpers.js'
+import 'sneat-bootstrap-html-admin-template-free/assets/vendor/js/menu.js'
+import 'sneat-bootstrap-html-admin-template-free/assets/js/main.js'
